944d60e39d88c55b5a9ef73118331f37bda841e6
devp2 <devp2@szkf.local>
2020-07-09 11:43:40 +0800

    add feature

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,86 @@
-// use std::process::Command;
+use std::env;
 use std::fs;
 use std::io;
-use std::path::{Path,PathBuf};
-use std::env;
+use std::io::Error;
+use std::path::{Path, PathBuf};
+// use std::thread;
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let p = PathBuf::from(".");
-    match args.get(1){
-        Some(str) => {
-            match find_file(p,&str) {
-                Ok(s) => println!("{}",s),
-                Err(err) => println!("Error: {:?}", err)
-            }
-        },
-        None => {
-            let s = String::from("*");
-            match find_file(p,&s) {
-                Ok(s) => println!("{}",s),
-                Err(err) => println!("Error: {:?}", err)
-            }
-        }
-    } 
+    // let p = PathBuf::from(".");
+    // let mut sn = String::new();
+    let mut args = Vec::new();
+    // match io::stdin().read_line(&mut sn) {
+    //     Ok(_) => {
+    //         println!("reading...");
+    //         let str_in = sn.trim().to_string();
+    //         match find_file(p,&str_in) {
+    //             Ok(s) => println!("{}",s),
+    //             Err(err) => println!("Error: {:?}", err)
+    //         }
+    //     },
+    //     Err(e) => {
+    //         println!("{}",e);
+    //     }
+    // };
+
+    for arg in env::args() {
+        args.push(arg);
+    }
+
+    // let res = parser_args(args);
+    match parser_args(args) {
+        Ok(_) => (),
+        Err(e) => println!("error:{}", e),
+    }
+    // println!("{:?}",args);
 }
 
+fn parser_args(args: Vec<String>) -> Result<String, Error> {
+    if args.len() > 1 {
+        match args[1].trim() {
+            "-r" => {
+                // println!("hell owlrd");
+                if args.len() >= 3 {
+                    let path = PathBuf::from(args[2].trim());
+                    let str_ser = &args[1];
+                    match find_file(path, &str_ser) {
+                        Ok(_) =>(),
+                        Err(err) => println!("Error: {:?}", err),
+                    }
+                } else {
+                    println!("参数不全");
+                }
 
-fn find_file(path: PathBuf,query: &String) -> io::Result<&str> {
+                return Ok("-r".to_string());
+            }
+            "-d" => {
+                // println!("fuck world");
+                return Ok("-d".to_string());
+            }
+            _ => {
+                // 起始路径
+                let path = PathBuf::from(".");
+                let str_ser = &args[1];
+                match find_file(path, &str_ser) {
+                    Ok(s) => println!("{}", s),
+                    Err(err) => println!("Error: {:?}", err),
+                }
+                return Ok("#".to_string());
+            }
+        }
+    } else {
+        Ok("*".to_string())
+    }
+}
+
+fn find_file(path: PathBuf, query: &String) -> io::Result<&str> {
     if let Ok(entries) = fs::read_dir(path) {
         for entry in entries {
             if let Ok(entry) = entry {
                 let strpath = entry.path();
                 // strpath.push("");
                 if Path::new(&strpath).is_dir() {
-                    find_file(strpath,query)?;
+                    find_file(strpath, query)?;
                 } else {
                     if !Path::new("DIR").exists() {
                         fs::create_dir("DIR")?;
@@ -46,7 +95,6 @@ fn find_file(path: PathBuf,query: &String) -> io::Result<&str> {
                     } else {
                         fs::copy(np, or)?;
                     }
-                    
                 }
             }
         }
